d8025ba027cbac2aae64957dc951ba59f7f90f9d
Lina Zoo <linazooper@gmail.com>
2017-12-13 18:32:41 -0600

    add link component to newpost route

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPosts } from '../actions';
-
 
 class PostsIndex extends Component {
     componentDidMount() {
@@ -24,6 +24,11 @@ class PostsIndex extends Component {
         console.log(this.props.posts);
         return (
             <div>
+                <div className="text-xs-right">
+                    <Link className="btn btn-primary" to="/posts/new">
+                        Add a Posts
+                    </Link>
+                </div>
                 <h3>Posts</h3>
                 <ul className="list-group">
                     { this.renderPosts() }
