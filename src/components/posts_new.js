60f8459d6c0dfd8b83dcf0ab39b79e2c50b02308
Lina Zoo <linazooper@gmail.com>
2017-12-14 21:50:34 -0600

    add onSubmit action that posts to the back end

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,14 +11,19 @@ class PostsNew extends Component {
                     type="text"
                     { ...field.input }
                 />
+                { field.meta.error }
             </div>
         );
     }
 
+    onSubmit(values) {
+        console.log(values);
+    }
 
     render() {
+        const { handleSubmit } = this.props;
         return (
-            <form>
+            <form onSubmit= { handleSubmit(this.onSubmit.bind(this)) }>
                 <Field 
                     label="Title for Post"
                     name="title"
@@ -34,6 +39,7 @@ class PostsNew extends Component {
                     name="content"
                     component= { this.renderField }
                 />
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     } 
