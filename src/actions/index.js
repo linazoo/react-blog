402c6dce49a6ebbc1712235196a9e25579861108
John Lyden <johnlydennc@gmail.com>
2017-12-27 16:03:16 -0600

    createPost action creator wired up Post request succesfullu making post request

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PPRCLIP1234'
@@ -12,5 +13,14 @@ export function fetchPosts()  {
     return {
         type: FETCH_POSTS,
         payload: request
+    };
+}
+
+export function createPost(values) {
+    const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+    return {
+        type: CREATE_POST,
+        payload: request 
     };
 }
